7b49687b0d4d1842c7caa26f551bd60f5ada635f
Wentao <wentaowilliam@outlook.com>
2024-02-23 22:49:07 -0500

    Add search menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import { AppBar, Toolbar, Box, IconButton, Badge } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  IconButton,
+  Badge,
+  InputBase,
+} from "@mui/material";
 import { NavLink } from "react-router-dom";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SearchIcon from "../assets/search.svg";
 import localforage from "localforage";
 import { useState, useEffect } from "react";
 import logo from "../assets/shopping.svg";
@@ -26,13 +34,48 @@ const useCartItems = () => {
 
 export default function Navbar({ mode, setMode }) {
   const cartItems = useCartItems();
+  const [search, setSearch] = useState("");
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    localforage.getItem("products").then((products) => {
+      setProducts(products || []);
+    });
+  }, []);
+  useEffect(() => {
+    setFilteredProducts(
+      products.filter((product) =>
+        product.title.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, products]);
   return (
     <AppBar position="static">
       <Toolbar>
         <Box display="flex" flexGrow={1}>
           <img className="logo" src={logo} alt="logo" />
+          <img className="logo" src={SearchIcon} alt="logo" />
+          <InputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          {search && (
+            <Box component="section">
+              {filteredProducts.slice(0, 5).map((product) => (
+                <NavLink
+                  key={product.id}
+                  to={`/product/${product.id}`}
+                  style={{ color: "inherit", textDecoration: "none" }}
+                >
+                  <p>{product.title}</p>
+                </NavLink>
+              ))}
+            </Box>
+          )}
         </Box>
-
         <NavLink
           to="/"
           style={{
